8b7e4571f9e9f6f339fda6f6af9617ece45be403
nourankhaled22 <145053863+nourankhaled22@users.noreply.github.com>
2024-07-15 20:58:16 +0300

    Update add-save-flashcard.dart

--- lib/add-save-flashcard.dart
+++ lib/add-save-flashcard.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'main.dart';
@@ -6,18 +5,17 @@ import 'main.dart';
 class AddFlashcardScreen extends StatelessWidget {
   final TextEditingController questionController = TextEditingController();
   final TextEditingController choicesController = TextEditingController();
-  final TextEditingController correctAnswerIndexController = TextEditingController();
+  final TextEditingController correctAnswerIndexController =
+  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Flashcard'),
       ),
       backgroundColor: Colors.grey[900],
       body: Padding(
-
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,24 +42,19 @@ class AddFlashcardScreen extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                _saveFlashcard(context);
-              },
+              onPressed: () => _saveFlashcard(context),
               child: Text(
                 'Save',
                 style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
-              style: ButtonStyle(
-                backgroundColor:
-                MaterialStateProperty.all<Color>(Color(0x615a57)
-                    .withOpacity(0.5)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
               ),
             ),
-
-
           ],
         ),
       ),
@@ -70,34 +63,45 @@ class AddFlashcardScreen extends StatelessWidget {
 
   void _saveFlashcard(BuildContext context) {
     final question = questionController.text;
-    final options = choicesController.text.split(',').map((choice) =>
-        choice.trim()).toList();
-    final correctOptionIndex = int.tryParse(
-        correctAnswerIndexController.text) ?? 0;
+    final options = choicesController.text
+        .split(',')
+        .map((choice) => choice.trim())
+        .toList();
+    final correctOptionIndex =
+        int.tryParse(correctAnswerIndexController.text) ?? 0;
 
-    if (question.isNotEmpty && options.isNotEmpty && correctOptionIndex >= 0 &&
+    if (question.isNotEmpty &&
+        options.isNotEmpty &&
+        correctOptionIndex >= 0 &&
         correctOptionIndex < options.length) {
-      Navigator.pop(context, Flashcard(question: question,
+      Navigator.pop(
+        context,
+        Flashcard(
+          question: question,
           options: options,
-          correctOptionIndex: correctOptionIndex));
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) =>
-            AlertDialog(
-              title: Text('Error'),
-              content: Text(
-                  'Please enter a valid question, options, and correct answer index.'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            ),
+          correctOptionIndex: correctOptionIndex,
+        ),
       );
+    } else {
+      _showErrorDialog(context);
     }
+  }
+
+  void _showErrorDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(
+          'Please enter a valid question, options, and correct answer index.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
 }
