f32cce20476d53db89d4635b67eef815e23d4193
nourankhaled22 <145053863+nourankhaled22@users.noreply.github.com>
2024-07-15 20:57:50 +0300

    Update add_flashcard_screen.dart

--- lib/add_flashcard_screen.dart
+++ lib/add_flashcard_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'flashcard.dart';
+import 'flashcard.dart'; // Import your necessary files
 import 'flashcard_model.dart';
 import 'glassmorphism_container.dart';
 
 class AddFlashcardScreen extends StatelessWidget {
   final TextEditingController questionController = TextEditingController();
   final TextEditingController optionsController = TextEditingController();
-  final TextEditingController correctOptionIndexController = TextEditingController();
+  final TextEditingController correctOptionIndexController =
+  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +17,19 @@ class AddFlashcardScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: SingleChildScrollView( // Wrap with SingleChildScrollView
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             buildTextField(controller: questionController, label: 'Question'),
             SizedBox(height: 20.0),
-            buildTextField(controller: optionsController, label: 'Options (comma separated)'),
+            buildTextField(
+                controller: optionsController, label: 'Options (comma separated)'),
             SizedBox(height: 20.0),
-            buildTextField(controller: correctOptionIndexController, label: 'Correct Option Index'),
+            buildTextField(
+                controller: correctOptionIndexController,
+                label: 'Correct Option Index'),
             SizedBox(height: 20.0),
             buildSaveButton(context),
             SizedBox(height: 20.0), // Optional SizedBox for additional spacing
@@ -35,14 +39,16 @@ class AddFlashcardScreen extends StatelessWidget {
     );
   }
 
-  Widget buildTextField({required TextEditingController controller, required String label}) {
+  Widget buildTextField(
+      {required TextEditingController controller, required String label}) {
     return GlassmorphismContainer(
       child: TextField(
         controller: controller,
         decoration: InputDecoration(
           labelText: label,
           border: InputBorder.none,
-          labelStyle: TextStyle(color: Colors.indigo, fontWeight: FontWeight.bold),
+          labelStyle:
+          TextStyle(color: Colors.indigo, fontWeight: FontWeight.bold),
         ),
         style: TextStyle(color: Colors.black),
       ),
@@ -53,11 +59,15 @@ class AddFlashcardScreen extends StatelessWidget {
     return GlassmorphismContainer(
       child: ElevatedButton(
         onPressed: () => saveFlashcard(context),
-        child: Text('Save', style: TextStyle(color: Colors.white, fontSize: 18.0,fontWeight: FontWeight.bold)),
+        child: Text('Save',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold)),
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+          backgroundColor: Colors.blue,
+          padding:
+          EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
         ),
       ),
     );
@@ -65,11 +75,23 @@ class AddFlashcardScreen extends StatelessWidget {
 
   void saveFlashcard(BuildContext context) {
     final question = questionController.text;
-    final options = optionsController.text.split(',').map((c) => c.trim()).toList();
-    final correctOptionIndex = int.tryParse(correctOptionIndexController.text) ?? 0;
+    final options =
+    optionsController.text.split(',').map((c) => c.trim()).toList();
+    final correctOptionIndex =
+        int.tryParse(correctOptionIndexController.text) ?? 0;
 
-    if (question.isNotEmpty && options.isNotEmpty && correctOptionIndex >= 0 && correctOptionIndex < options.length) {
-      Navigator.pop(context, Flashcard(question: question, options: options, correctOptionIndex: correctOptionIndex));
+    if (question.isNotEmpty &&
+        options.isNotEmpty &&
+        correctOptionIndex >= 0 &&
+        correctOptionIndex < options.length) {
+      Navigator.pop(
+        context,
+        Flashcard(
+          question: question,
+          options: options,
+          correctOptionIndex: correctOptionIndex,
+        ),
+      );
     } else {
       showDialog(
         context: context,
